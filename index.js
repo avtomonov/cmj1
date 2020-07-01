b0e9d836dc4f355dcab21aa16a007b412588d371
Andrew <web@hu2.ru>
2020-07-01 21:01:01 +0300

    'sdsу'

--- index.js
+++ index.js
@@ -8,47 +8,13 @@ app.listen(PORT, () => {
 	console.log('started server')
 })
 
-const app_id_insales = 'e430f992ca01bc61286b1e50bf58c57c'
-const app_key_insales = '17b8a11d15946f5aaaf39aaa4278aa9b'
-const api = {};
-
-api.changeStatus = (order_id, custom_status_permalink = 'accepted') => {
-	let status = {
-	  "order": {
-	    "custom_status_permalink": `${custom_status_permalink}`
-	  }
-	}	
-	let headersOpt = {  
-    	"Content-type": "application/json",
-	};
-	request(
-	        {
-	        method:'put',
-	        url:`https://${app_id_insales}:${app_key_insales}@chesnokov-lox.myinsales.ru/admin/orders/${order_id}.json`, 
-	        body: status, 
-	        headers: headersOpt,
-	        json: true,
-	    }, function (error, response, body) {  
-	        //Print the Response
-	        console.log(body);  
-	        	let send = JSON.stringify(response)	
-			res.send(`
-			   	<h1>Замена статусов заказа инсейлс</h1>
-				<div>${send}</div>
-			`);	
-	}); 
-}
+const id = 'e430f992ca01bc61286b1e50bf58c57c'
+const key = '17b8a11d15946f5aaaf39aaa4278aa9b'
 
 
 
 app.get('/', (req, res) => {
-	api.changeStatus(21197247)    
-});
-
-
-
-
-
+    
 
 	// request(`https://${id}:${key}@chesnokov-lox.myinsales.ru/admin/orders.json`, (err, response, body) => {
 	//    let send = JSON.stringify(response)	
@@ -70,11 +36,34 @@ app.get('/', (req, res) => {
 
 
 
+let status = {
+  "order": {
+    "custom_status": "accepted"
+  }
+}
 
 
 
 //Custom Header pass
-
+var headersOpt = {  
+    "Content-Type": "application/json",
+};
+request(
+        {
+        method:'put',
+        url:`https://${id}:${key}@chesnokov-lox.myinsales.ru/admin/orders/21197247.json`, 
+        body: status, 
+        headers: headersOpt,
+        json: true,
+    }, function (error, response, body) {  
+        //Print the Response
+        console.log(body);  
+        	let send = JSON.stringify(response)	
+		res.send(`
+		   	<h1>Замена статусов заказа инсейлс</h1>
+			<div>${send}</div>
+		`);	
+}); 
 
 
 // request({ url: `https://${id}:${key}@chesnokov-lox.myinsales.ru/admin/orders/21197247.json`,headers: {'content-type' : 'application/json'}, method: 'PUT', form: status}, (err, response, body) =>{
@@ -95,3 +84,11 @@ app.get('/', (req, res) => {
  //     console.log(err, response, body);
     
  //    });
+
+
+});
+
+
+
+
+
