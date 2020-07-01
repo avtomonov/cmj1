5918c24af4d70cce53dda64c79c462088ef0fa71
Andrew <web@hu2.ru>
2020-07-01 19:23:37 +0300

    deploy2

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const request = require('request')
 const app = express()
 
 const PORT = process.env.PORT || 80
@@ -9,6 +10,19 @@ app.listen(PORT, () => {
 
 app.get('/', (req, res) => {
     res.send(`<h1>Замена статусов заказа инсейлс</h1>`);
+});
+
+const id = 'e430f992ca01bc61286b1e50bf58c57c'
+const key = '17b8a11d15946f5aaaf39aaa4278aa9b'
+
+
+request(`https://${id}:${key}@chesnokov-lox.myinsales.ru/admin/orders.xml`, (err, response, body) => {
+    if(err){
+		return res.status(500).send({message: err});
+    }
+    else{
+    	return res.send(body);
+    }  
 });
 
 
