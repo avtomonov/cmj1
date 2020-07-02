e08973e3af16dbe95a5ef924194093b5a5ee9401
Andrew <web@hu2.ru>
2020-07-03 01:55:39 +0300

    'd3213'

--- index.js
+++ index.js
@@ -3,11 +3,6 @@ const request = require('request')
 const MongoClient = require('mongodb').MongoClient;
 const app = express()
 
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.urlencoded());
-
-
 const PORT = process.env.PORT || 80
 
 app.listen(PORT, () => {
@@ -20,53 +15,67 @@ const key = '17b8a11d15946f5aaaf39aaa4278aa9b'
 
 const uri = "mongodb+srv://andrevv:qwe321@cluster0.qc9y5.mongodb.net/<dbname>?retryWrites=true&w=majority";
 
-
-const client = new MongoClient(uri, { useUnifiedTopology: true } );
-
-client.connect(err => {
-	// const collection = client.db("test").collection("devices");
- // 	collection.insertOne(
- //     {id: 2, login: 'login2', name: 'name1', gender: 'male'},
- //     (err, result) => {
- //       if(err){
- //         console.log('Unable insert user: ', err);
- //         throw err;
- //       }
- //     }
- //    );
-  // perform actions on the collection object
-  client.close();
-
-
-
-
-});
-
-console.log(client)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.urlencoded());
 
 app.post('/url', function(req, res) {
     const url = req.body.url;
     res.send(url);
 });
+
+
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+
+// client.connect(err => {
+// 	const collection = client.db("test").collection("devices");
+//  	collection.insertOne(
+//      {id: 2, login: 'login2', name: 'name1', gender: 'male'},
+//      (err, result) => {
+//        if(err){
+//          console.log('Unable insert user: ', err);
+//          throw err;
+//        }
+//      }
+//     );
+//   // perform actions on the collection object
+//   client.close();
+
+
+
+
+// });
+
+// console.log(client)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 app.get('/', (req, res) => {
@@ -113,11 +122,11 @@ request(
         json: true,
     }, function (error, response, body) {  
         //Print the Response
-  //       let send = JSON.stringify(response)	
-		// res.send(`
-		//    	<h1>Замена статусов заказа инсейлс</h1>
-		// 	<div>${send}</div>
-		// `);	
+        let send = JSON.stringify(response)	
+		res.send(`
+		   	<h1>Замена статусов заказа инсейлс</h1>
+			<div>${send}</div>
+		`);	
 }); 
 
 
