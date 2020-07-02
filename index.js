bb97e62530ba16cad88cec0642ad161ec232e18e
Andrew <web@hu2.ru>
2020-07-03 01:15:00 +0300

    'dadad'

--- index.js
+++ index.js
@@ -16,19 +16,19 @@ const key = '17b8a11d15946f5aaaf39aaa4278aa9b'
 const uri = "mongodb+srv://andrevv:qwe321@cluster0.qc9y5.mongodb.net/<dbname>?retryWrites=true&w=majority";
 
 
-const client = new MongoClient(uri, { useNewUrlParser: true });
+const client = new MongoClient(uri, { useUnifiedTopology: true } );
 
 client.connect(err => {
-	const collection = client.db("test").collection("devices");
- 	collection.insertOne(
-     {id: 2, login: 'login2', name: 'name1', gender: 'male'},
-     (err, result) => {
-       if(err){
-         console.log('Unable insert user: ', err);
-         throw err;
-       }
-     }
-    );
+	// const collection = client.db("test").collection("devices");
+ // 	collection.insertOne(
+ //     {id: 2, login: 'login2', name: 'name1', gender: 'male'},
+ //     (err, result) => {
+ //       if(err){
+ //         console.log('Unable insert user: ', err);
+ //         throw err;
+ //       }
+ //     }
+ //    );
   // perform actions on the collection object
   client.close();
 
@@ -64,7 +64,9 @@ console.log(client)
 
 
 
-
+app.post('/database', (req, res) => {
+	res.end(req, res);
+})
 
 
 
