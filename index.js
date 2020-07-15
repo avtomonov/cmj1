a76318e096a78b098b2be0a3064af7092803c5cd
Andrew <web@hu2.ru>
2020-07-15 15:54:56 +0300

    'ad'

--- index.js
+++ index.js
@@ -19,12 +19,18 @@ mongoose.connect(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
-
-
-
 const cors = require('cors');
-app.use(cors());
-app.options('*', cors());
+
+var whitelist = ['http://www.cmjournal.ru', 'http://example2.com']
+var corsOptionsDelegate = function (req, callback) {
+  var corsOptions;
+  if (whitelist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+  }else{
+    corsOptions = { origin: false } // disable CORS for this request
+  }
+  callback(null, corsOptions) // callback expects two parameters: error and options
+}
 
 app.listen(PORT, () => {
   console.log("started server");
